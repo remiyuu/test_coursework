edc21fce26e4b02a8e76a91bd3393e4cbdc5fcf4
anton <148725911+remiyuu@users.noreply.github.com>
2024-05-30 18:27:14 +0300

    Update lxkckcl.c

--- lxkckcl.c
+++ lxkckcl.c
@@ -83,3 +83,132 @@ int main() {
     getchar();
     return 0;
 }
+
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_NONSTDC_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <stdbool.h>
+
+void main(void) {
+    char src_num[] = "-15678";
+    char res_num[50];
+    int num = 0;
+    char* pstr;
+    char digit;
+    bool negative = false;
+    int decimal_num = 0;
+    int base = 1;
+
+    pstr = src_num;
+
+    __asm {
+        push eax;
+        push ebx;
+        push ecx;
+        push edx;
+
+        mov num, 0
+            lea eax, src_num
+            mov pstr, eax
+
+            LabelStart1 :
+        // Проверка конца строки
+        mov eax, pstr
+            mov cl, byte ptr[eax]
+            cmp cl, '-'
+            jne LabelContinueCalc
+            mov negative, 1
+            jmp LabelNextChar
+            LabelContinueCalc :
+        test cl, cl
+            je LabelEnd1
+            // Преобразование символа в число
+            cmp cl, '0'
+            jl LabelNextChar
+            cmp cl, '8'
+            jg LabelNextChar
+            sub cl, '0'
+            mov digit, cl
+            // Формирование числа num
+            mov eax, num
+            mov ebx, 9
+            mul ebx
+            xor ebx, ebx
+            mov bl, digit
+            add eax, ebx
+            mov num, eax
+            // Переход к следующему символу строки
+            LabelNextChar :
+        inc pstr
+            jmp LabelStart1
+
+            LabelEnd1 :
+        // Преобразование числа в строку
+        lea eax, res_num;
+        add eax, 49;
+        mov pstr, eax;
+        mov byte ptr[eax], 0;
+
+    LabelStart2:
+        // Проверка окончания цикла
+        cmp num, 0
+            jle LabelEnd2
+            // Получение младшей цифры числа
+            mov eax, num
+            xor edx, edx
+            mov ebx, 2
+            div ebx
+            mov num, eax
+
+            // Преобразование цифры в символ
+            add dl, '0'
+            mov digit, dl
+            dec pstr
+            mov eax, pstr
+            mov byte ptr[eax], dl
+
+            // Update decimal_num and base
+            mov eax, dword ptr[decimal_num]
+            mov ecx, dword ptr[base]
+            mul ecx
+            add eax, edx
+            mov dword ptr[decimal_num], eax
+            mov eax, dword ptr[base]
+            mov ecx, 2
+            mul ecx
+            mov dword ptr[base], eax
+
+            jmp LabelStart2;
+
+    LabelEnd2:
+        // If the number is negative, we need to subtract the decimal equivalent from 2^32.
+        cmp byte ptr[negative], 0
+        je LabelPrint
+        mov eax, 1
+        mov ecx, 32
+        LabelPow:
+            mul eax
+            loop LabelPow
+        mov ecx, dword ptr[decimal_num]
+        sub eax, ecx
+        mov dword ptr[decimal_num], eax
+
+    LabelPrint:
+        // Print the results
+        pop edx;
+        pop ecx;
+        pop ebx;
+        pop eax;
+
+        printf("9 -> 2\nsource = ");
+        for (int i = 0; src_num[i] != '\0'; i++) {
+            printf("%c", src_num[i]);
+        }
+        printf("\nresult = ");
+        printf(negative ? "-%s\n" : "%s\n", pstr);
+        printf("Decimal equivalent = %d\n", decimal_num);
+    }
+}
