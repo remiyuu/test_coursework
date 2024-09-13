05857340c981b61c583304872dc7969932e2670c
anton <148725911+remiyuu@users.noreply.github.com>
2024-09-13 16:16:07 +0300

    Update l2or.c

--- l2or.c
+++ l2or.c
@@ -1,45 +1,52 @@
-void shell(int *items, int count)
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
 {
+	setvbuf(stdin, NULL, _IONBF, 0);
+	setvbuf(stdout, NULL, _IONBF, 0);
 
-  int i, j, gap, k;
-  int x, a[5];
+	clock_t start, end; // объявляем переменные для определения времени выполнения
 
-  a[0]=9; a[1]=5; a[2]=3; a[3]=2; a[4]=1;
+	int i=0, j=0, r;
+	int a[200][200], b[200][200], c[200][200], elem_c;
 
-  for(k=0; k < 5; k++) {
-    gap = a[k];
-    for(i=gap; i < count; ++i) {
-      x = items[i];
-      for(j=i-gap; (x < items[j]) && (j >= 0); j=j-gap)
-        items[j+gap] = items[j];
-      items[j+gap] = x;
-    }
-  }
+	srand(time(NULL)); // инициализируем параметры генератора случайных чисел
+	while(i<200)
+	{
+		while(j<200)
+		{
+			a[i][j]=rand()% 100 + 1; // заполняем массив случайными числами
+			j++;
+		}
+		i++;
+	}
+	srand(time(NULL)); // инициализируем параметры генератора случайных чисел
+	i=0; j=0;
+	while(i<200)
+	{
+		while(j<200)
+		{
+			b[i][j]=rand()% 100 + 1; // заполняем массив случайными числами
+			j++;
+		}
+		i++;
+	}
+
+	for(i=0;i<200;i++)
+	{
+		for(j=0;j<200;j++)
+		{
+			elem_c=0;
+			for(r=0;r<200;r++)
+			{
+				elem_c=elem_c+a[i][r]*b[r][j];
+				c[i][j]=elem_c;
+			}
+		}
+	}
+
+
+	return(0);
 }
-
-void qs(int *items, int left, int right) //вызов функции: qs(items, 0, count-1);
-{
-  int i, j;
-  int x, y;
-
-
-
-  i = left; j = right;
-
-  /* выбор компаранда */
-  x = items[(left+right)/2];
-  
-  do {
-    while((items[i] < x) && (i < right)) i++;
-    while((x < items[j]) && (j > left)) j--;
-
-    if(i <= j) {
-      y = items[i];
-      items[i] = items[j];
-      items[j] = y;
-      i++; j--;
-    }
-  } while(i <= j);
-
-  if(left < j) qs(items, left, j);
-  if(i < right) qs(items, i, right);
